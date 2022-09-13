faaca2a1ed1679f016283d3a13b920399e8cbe48
Hasan VAROL <hasanvarooll@gmail.com>
2022-09-14 02:16:11 +0300

    Add project files.

---
+++ KitapSatis/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace KitapSatis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txtBx1 = new System.Windows.Forms.TextBox();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(659, 110);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel2.Location = new System.Drawing.Point(0, 301);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(659, 79);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(125, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kitap Sipariş Paneli";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(210, 153);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(81, 17);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "Kitap Sayısı:";
+            // 
+            // txtBx1
+            // 
+            this.txtBx1.Location = new System.Drawing.Point(297, 150);
+            this.txtBx1.Name = "txtBx1";
+            this.txtBx1.Size = new System.Drawing.Size(100, 25);
+            this.txtBx1.TabIndex = 3;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(249, 193);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(42, 17);
+            this.lbl3.TabIndex = 4;
+            this.lbl3.Text = "Tutar:";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(297, 193);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(22, 17);
+            this.lbl4.TabIndex = 5;
+            this.lbl4.Text = "00";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(297, 227);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(100, 32);
+            this.btn1.TabIndex = 7;
+            this.btn1.Text = "Hesapla";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 384);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.txtBx1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Kitap Satın Alma Paneli";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txtBx1;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ KitapSatis/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KitapSatis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            double kitapsayisi, kitapfiyati = 8, toplamfiyat;
+            kitapsayisi = Convert.ToDouble(txtBx1.Text);
+            if (kitapsayisi >= 0 && kitapsayisi <= 20)
+            {
+                toplamfiyat = (kitapsayisi * kitapfiyati) - ((kitapsayisi * kitapfiyati) * 0.2);
+                lbl4.Text = toplamfiyat.ToString() + " TL";
+            }
+            else if (kitapsayisi >= 21 && kitapsayisi <= 40)
+            {
+                toplamfiyat = (kitapsayisi * kitapfiyati) - ((kitapsayisi * kitapfiyati) * 0.2);
+                lbl4.Text = toplamfiyat.ToString() + " TL";
+            }
+            else if (kitapsayisi >= 41)
+            {
+                toplamfiyat = (kitapsayisi * kitapfiyati) - ((kitapsayisi * kitapfiyati) * 0.2);
+                lbl4.Text = toplamfiyat.ToString() + " TL";
+            }
+            else
+            {
+                lbl4.Text = "Geçerli Adet giriniz.(Sayma Sayısı.)";
+            }
+        }
+    }
+}
+// 0-20 --> % 20
+// 21-40--> % 40
+// 41++ --> % 50
+// Kitap = 8
